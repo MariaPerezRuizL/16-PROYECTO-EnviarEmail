1fed026912f6c7fefc33bb6ac7417865f4d77115
María Pérez Ruiz de Larrinaga <MPerezR@kabel.es>
2021-02-15 16:47:28 +0100

    disabled button using tailwind library

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 //Variables
 
+const btnSend=document.getElementById("enviar")
+
 //eventListeners
 
+eventListeners();
 function eventListeners(){
     //Add de load of main app
     document.addEventListener('DOMContentLoaded', initializeApp);
+
 }
 
 //Functions
 
 function initializeApp(){
     console.log("App initializating...");
+    btnSend.disabled=true;
+    btnSend.classList.add("cursor-not-allowed", "opacity-50"); // Clases de tailwind
 }
