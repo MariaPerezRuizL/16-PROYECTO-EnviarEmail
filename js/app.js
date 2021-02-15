f4a0f69894c4747b89702abe24e635984e8661cc
María Pérez Ruiz de Larrinaga <MPerezR@kabel.es>
2021-02-15 17:11:25 +0100

    add empty validation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 //Variables
-
 const btnSend=document.getElementById("enviar")
+
+//VAriables form 
+const email=document.getElementById("email");
+const asunto=document.getElementById("asunto");
+const message=document.getElementById("mensaje");
 
 //eventListeners
 
@@ -8,6 +12,11 @@ eventListeners();
 function eventListeners(){
     //Add de load of main app
     document.addEventListener('DOMContentLoaded', initializeApp);
+
+    //Form fields validation
+    email.addEventListener('blur', validateForm)
+    asunto.addEventListener('blur', validateForm)
+    message.addEventListener('blur', validateForm)
 
 }
 
@@ -17,4 +26,20 @@ function initializeApp(){
     console.log("App initializating...");
     btnSend.disabled=true;
     btnSend.classList.add("cursor-not-allowed", "opacity-50"); // Clases de tailwind
+}
+
+function validateForm(e){
+    //console.log("Validando...")
+    //obtenemos el valor del elemento sobre el que se ha producido el evento 
+    //Blur: se dispara cuando salimos del elemento
+    console.log(e.target.value)
+    if(e.target.value.length>0){
+
+    }
+    else{
+        //e.target.style.borderBottomColor='red'; css utilizando js
+
+        //utilizamos tailwind
+        e.target.classList.add('border', 'border-red-500');
+    }
 }
